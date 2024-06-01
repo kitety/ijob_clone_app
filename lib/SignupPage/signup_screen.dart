f3f874788a4bf5c8201bf22c915d2845c6c1a69a
kitety <aizaizuori@foxmail.com>
2024-06-02 00:13:35 +0800

    feat: 15 user avatar container

--- lib/SignupPage/signup_screen.dart
+++ lib/SignupPage/signup_screen.dart
@@ -1,5 +1,6 @@
+import 'dart:io';
+
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../Services/global_variables.dart';
@@ -15,21 +16,13 @@ class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
   late Animation<double> _animation;
   late AnimationController _animationController;
 
-  final TextEditingController _emailController =
-      TextEditingController(text: '');
-  final TextEditingController _passwordController =
-      TextEditingController(text: '');
-  final FocusNode _passFocusNode = FocusNode();
-  final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final GlobalKey<FormState> _signupFormKey = GlobalKey<FormState>();
+  File? imageFile;
   bool _obscureText = false;
 
   @override
   void dispose() {
     _animationController.dispose();
-    _emailController.dispose();
-    _passwordController.dispose();
-    _passFocusNode.dispose();
     super.dispose();
   }
 
@@ -54,6 +47,7 @@ class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
         children: [
@@ -68,7 +62,57 @@ class _SignUpState extends State<SignUp> with TickerProviderStateMixin {
             height: double.infinity,
             fit: BoxFit.cover,
             alignment: FractionalOffset(_animation.value, 0),
-          )
+          ),
+          Container(
+            color: Colors.black45,
+            width: double.infinity,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 80),
+              child: ListView(
+                children: [
+                  Form(
+                    key: _signupFormKey,
+                    child: Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            // create ShowImageDialog
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              width: size.width * 0.24,
+                              height: size.width * 0.24,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  width: 1,
+                                  color: Colors.cyan,
+                                ),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(16),
+                                child: imageFile == null
+                                    ? const Icon(
+                                        Icons.camera_enhance_sharp,
+                                        size: 30,
+                                        color: Colors.cyan,
+                                      )
+                                    : Image.file(
+                                        imageFile!,
+                                        fit: BoxFit.fill,
+                                      ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
