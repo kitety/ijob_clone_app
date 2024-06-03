b06054cc40165e329bef473b003c66a9fef914e4
kitety <aizaizuori@foxmail.com>
2024-06-03 23:21:09 +0800

    feat: 29 Forgot Password Firebase

--- lib/ForgetPassword/forget_password_screen.dart
+++ lib/ForgetPassword/forget_password_screen.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:ijob_clone_app/LoginPage/login_screen.dart';
 import 'package:ijob_clone_app/Services/global_variables.dart';
 
 class ForgetPassword extends StatefulWidget {
@@ -17,20 +19,14 @@ class _ForgetPasswordState extends State<ForgetPassword>
 
   final TextEditingController _emailController =
       TextEditingController(text: '');
-  final TextEditingController _passwordController =
-      TextEditingController(text: '');
-  final FocusNode _passFocusNode = FocusNode();
-  final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-  bool _obscureText = false;
   bool _isLoading = false;
 
   @override
   void dispose() {
     _animationController.dispose();
     _emailController.dispose();
-    _passwordController.dispose();
-    _passFocusNode.dispose();
     super.dispose();
   }
 
@@ -113,34 +109,73 @@ class _ForgetPasswordState extends State<ForgetPassword>
                   ),
                 ),
                 const SizedBox(height: 60),
-                MaterialButton(
-                  minWidth: double.infinity,
-                  onPressed: () {
-                    // Create forget password form
-                  },
-                  color: Colors.cyan,
-                  elevation: 8,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(13),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 14),
-                    child: Text(
-                      'Reset Now',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        fontStyle: FontStyle.italic,
-                      ),
-                    ),
-                  ),
-                )
+                _isLoading
+                    ? const Center(
+                        child: SizedBox(
+                          width: 70,
+                          height: 70,
+                          child: CircularProgressIndicator(),
+                        ),
+                      )
+                    : MaterialButton(
+                        minWidth: double.infinity,
+                        onPressed: () {
+                          // Create forget password form
+                          _forgetOPassSubmitForm();
+                        },
+                        color: Colors.cyan,
+                        elevation: 8,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(13),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 14),
+                          child: Text(
+                            'Reset Now',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              fontStyle: FontStyle.italic,
+                            ),
+                          ),
+                        ),
+                      )
               ],
             ),
           ),
         ],
       ),
     );
+  }
+
+  void _forgetOPassSubmitForm() async {
+    if (_emailController.text.isEmpty) {
+      Fluttertoast.showToast(msg: 'Please Input Your Email!');
+      return;
+    }
+    try {
+      setState(() {
+        _isLoading = true;
+      });
+      await _firebaseAuth.sendPasswordResetEmail(email: _emailController.text);
+      if (mounted) {
+        Fluttertoast.showToast(msg: 'Reset Password Link Sent To Your Email');
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const Login();
+            },
+          ),
+        );
+      }
+    } catch (e) {
+      Fluttertoast.showToast(msg: e.toString());
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 }
