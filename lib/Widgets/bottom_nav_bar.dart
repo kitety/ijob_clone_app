512f2e54427b1e1e0e0c971d2bd4dc4be0772935
kitety <aizaizuori@foxmail.com>
2024-06-11 19:28:31 +0800

    feat: 36 sign out the user

--- lib/Widgets/bottom_nav_bar.dart
+++ lib/Widgets/bottom_nav_bar.dart
@@ -1,8 +1,10 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ijob_clone_app/Jobs/upload_job.dart';
 import 'package:ijob_clone_app/Search/profile_company.dart';
 import 'package:ijob_clone_app/Search/search_companies.dart';
+import 'package:ijob_clone_app/user_state.dart';
 
 import '../Jobs/jobs_screen.dart';
 
@@ -10,6 +12,77 @@ class BottomNavigationBarForApp extends StatelessWidget {
   final int indexNum;
 
   const BottomNavigationBarForApp({super.key, required this.indexNum});
+
+  void _logout(BuildContext context) {
+    final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Colors.black45,
+            title: const Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Icon(
+                    Icons.logout,
+                    color: Colors.white,
+                    size: 36,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(
+                    'Sign Out',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            content: const Text(
+              'Do you want to logout?',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.canPop(context) ? Navigator.pop(context) : null;
+                },
+                child: const Text(
+                  'No',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  firebaseAuth.signOut();
+                  Navigator.canPop(context) ? Navigator.pop(context) : null;
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const UserState()),
+                  );
+                },
+                child: const Text(
+                  'Yes',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +99,7 @@ class BottomNavigationBarForApp extends StatelessWidget {
         Icon(Icons.search, size: 19, color: Colors.black),
         Icon(Icons.add, size: 19, color: Colors.black),
         Icon(Icons.person_pin, size: 19, color: Colors.black),
+        Icon(Icons.exit_to_app, size: 19, color: Colors.black),
       ],
       onTap: (index) {
         if (index == 0) {
@@ -67,6 +141,9 @@ class BottomNavigationBarForApp extends StatelessWidget {
               },
             ),
           );
+        }
+        if (index == 4) {
+          _logout(context);
         }
       },
     );
