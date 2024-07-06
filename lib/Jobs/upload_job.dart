de606b0c297193464de3b1eeeeff74d3071eb37d
kitety <aizaizuori@foxmail.com>
2024-07-06 17:56:34 +0800

    feat: 38&39 select job category form

--- lib/Jobs/upload_job.dart
+++ lib/Jobs/upload_job.dart
@@ -10,6 +10,79 @@ class UploadJobNow extends StatefulWidget {
 }
 
 class _UploadJobNowState extends State<UploadJobNow> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _jobCategoryController =
+      TextEditingController(text: 'Select Job Category');
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _priceController = TextEditingController();
+  final TextEditingController _locationController = TextEditingController();
+
+  Widget _textTitles({required String label}) {
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: Text(
+        label,
+        style: const TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget _textFormFields({
+    required String valueKey,
+    required TextEditingController controller,
+    required bool enable,
+    required Function fct,
+    required int maxLength,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: InkWell(
+        onTap: () {
+          fct();
+        },
+        child: TextFormField(
+          validator: (value) {
+            if (value!.isEmpty) {
+              return 'Value is missing';
+            }
+            return null;
+          },
+          controller: controller,
+          enabled: enable,
+          key: ValueKey(valueKey),
+          style: const TextStyle(color: Colors.white),
+          maxLines: valueKey == 'JobDescription' ? 3 : 1,
+          maxLength: maxLength,
+          keyboardType: TextInputType.text,
+          decoration: const InputDecoration(
+            counterText: "",
+            filled: true,
+            fillColor: Colors.black54,
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.black,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.black,
+              ),
+            ),
+            errorBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.red,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -48,27 +121,52 @@ class _UploadJobNowState extends State<UploadJobNow> {
               ),
             ),
           ),
-          body: const Center(
+          body: Center(
             child: Padding(
-              padding: EdgeInsets.all(7.0),
+              padding: const EdgeInsets.all(7.0),
               child: Card(
                 color: Colors.white10,
                 child: SingleChildScrollView(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      SizedBox(height: 10),
-                      Align(
+                      const SizedBox(height: 10),
+                      const Align(
                         alignment: Alignment.center,
                         child: Padding(
                           padding: EdgeInsets.all(8.0),
                           child: Text(
                             'Please fill all fields',
                             style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 40,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Signatra'),
+                              color: Colors.black,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Signatra',
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Divider(
+                        thickness: 1,
+                        color: Colors.black45,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              _textTitles(label: 'Job Category :'),
+                              _textFormFields(
+                                valueKey: 'JobCategory',
+                                controller: _jobCategoryController,
+                                enable: false,
+                                fct: () {},
+                                maxLength: 100,
+                              )
+                            ],
                           ),
                         ),
                       )
